8cde11f4052a36aaa9eaf3c41ca3e8f7c0d5a8a9
b10300 <b10300@b10300-desktop.(none)>
2011-02-03 19:38:02 +0200

    Defined poll APIs. Started system test application.

--- sec-driver/tests/system-tests/test-scenario-1/test_sec_driver_poll.c
+++ sec-driver/tests/system-tests/test-scenario-1/test_sec_driver_poll.c
@@ -37,7 +37,7 @@ extern "C" {
 /*=================================================================================================
                                         INCLUDE FILES
 ==================================================================================================*/
-
+#include <stdio.h>
 #include "fsl_sec.h"
 
 /*==================================================================================================
@@ -71,106 +71,54 @@ extern "C" {
 /*==================================================================================================
                                      GLOBAL FUNCTIONS
 ==================================================================================================*/
+//sec_out_cbk pdcp_ready_packet_handler;
 
-int sec_init(int job_rings_no, sec_job_ring_t **job_ring_handles)
+int main(void)
 {
-	// stub function
-	return SEC_SUCCESS;
+    int ret = 0;
+    int job_rings_no = 2;
+    sec_job_ring_t *job_ring_handles[2];
+    sec_context_handle_t pdcp_ctx_handle = NULL;
+    sec_pdcp_context_info_t pdcp_ctx_cfg_data;
+
+    /////////////////////////////////////////////////////////////////////
+    // 1. Initialize SEC user space driver requesting 2 Job Rings
+    /////////////////////////////////////////////////////////////////////
+    ret = sec_init(job_rings_no, (sec_job_ring_t**)&job_ring_handles);
+    if (ret != SEC_SUCCESS)
+    {
+        printf("sec_init::Error %d", ret);
+        return 1;
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    // 2. Create a PDCP context affined to first Job Ring.
+    /////////////////////////////////////////////////////////////////////
+    ret = sec_create_pdcp_context (job_ring_handles[0],
+                                   &pdcp_ctx_cfg_data,
+                                   &pdcp_ctx_handle);
+    if (ret != SEC_SUCCESS)
+    {
+        printf("sec_create_pdcp_context::Error %d", ret);
+        return 1;
+    }
+
+
+
+    /////////////////////////////////////////////////////////////////////
+    // x. Initialize SEC user space driver requesting 2 Job Rings
+    /////////////////////////////////////////////////////////////////////
+
+    ret = sec_release();
+    if (ret != SEC_SUCCESS)
+    {
+        printf("sec_release::Error %d", ret);
+        return 1;
+    }
+
+    return 0;
 }
 
-int sec_release()
-{
-	// stub function
-	return SEC_SUCCESS;
-}
-
-int sec_create_pdcp_context (sec_job_ring_t job_ring_handle,
-                             sec_pdcp_context_info_t *sec_ctx_info, 
-                             sec_context_handle_t *sec_ctx_handle)
-{
-    // stub function
-
-    // 1. ret = get_free_context();
-
-    // No free contexts available in free list. 
-    // Maybe there are some retired contexts that can be recycled.
-    // Run context garbage collector routine.
-    //if (ret != 0)
-    //{
-    //    ret = collect_retired_contexts();
-    //}
-
-    //if (ret != 0 )
-    //{
-    //    return error, there are no free contexts
-    //}
-
-    // 2. Associate context with job ring
-    // 3. Update context with PDCP info from UA, create shared descriptor
-    // 4. return context handle to UA
-    // 5. Run context garbage collector routine
-
-	return SEC_SUCCESS;
-}
-
-int sec_delete_pdcp_context (sec_context_handle_t sec_ctx_handle)
-{
-    // stub function
-
-    // 1. Mark context as retiring
-    // 2. If context.packet_count == 0 then move context from in-use list to retiring list. 
-    //    Else move to retiring list
-    // 3. Run context garbage collector routine
-
-    return SEC_SUCCESS;
-}
-
-#if FSL_SEC_WORKING_MODE == FSL_SEC_POLLING_MODE
-int sec_poll(int32_t limit, uint32_t weight, uint32_t *packets_no)
-{
-    // 1. call sec_hw_poll() to check directly SEC's Job Rings for ready packets.
-    //
-    // sec_hw_poll() will do:
-    // a. SEC 3.1 specific: 
-    //      - poll for errors on /dev/sec_uio_x. Raise error notification to UA if this is the case.
-    // b. for all owned job rings:
-    //      - check and notify ready packets.
-    //      - decrement packet reference count per context
-    //      - other 
-    return SEC_SUCCESS;
-}
-int sec_poll_job_ring(sec_job_ring_t job_ring_handle, int32_t limit, uint32_t *packets_no)
-{
-    // 1. call sec_hw_poll_job_ring() to check directly SEC's Job Ring for ready packets.
-    return SEC_SUCCESS;
-}
-#elif FSL_SEC_WORKING_MODE == FSL_SEC_INTERRUPT_MODE
-int sec_poll(int32_t limit, uint32_t weight, uint32_t *packets_no)
-{
-    // 1. Start software poll on device files registered for all job rings owned by this user application.
-    //    Return if no IRQ generated for job rings.
-    // 
-    // Set timeout = 0 so that it returns immediatelly if no irq's are generated for any job ring.
-    // Timeout is expressed as miliseconds. Considering LTE TTI = 1 ms and the fact that the calling thread
-    // may do other processing tasks, a timeout other than 0 does not seem like an option.
-    //
-    // NOTE: epoll is more efficient than poll for a large number of file descriptors ~ 1000 ... 100000.
-    //       Considering SEC user space driver will poll for max 4 file descriptors, poll() is used instead of epoll().
-    //
-    // int poll(struct pollfd *fds, nfds_t nfds, int timeout);
-    //
-    //2. call sec_hw_poll() to check directly SEC's Job Rings for ready packets.
-
-    return SEC_SUCCESS;
-}
-int sec_poll_job_ring(sec_job_ring_t job_ring_handle, int32_t limit, uint32_t *packets_no)
-{
-    // 1. Start software poll on device file registered for this job ring.
-    //    Return if no IRQ generated for job ring.
-    // 2. call sec_hw_poll_job_ring() to check directly SEC's Job Ring for ready packets.
-    return SEC_SUCCESS;
-}
-#endif
 /*================================================================================================*/
 
 #ifdef __cplusplus
